2013f39a8d58a1b0c84395b0c82d1d8187b041c9
Monkie <a_brine@yahoo.com>
2023-09-26 19:36:19 +0100

    chore: update packages/core/src/index.ts
    added export all helpers functions

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,5 @@
 import ClientApi from "./client/client";
+export * as Helpers from "./utils/helpers";
 import EmbedBuilder from "./utils/embedBuilder";
 export * from "types";
-
 export { ClientApi, EmbedBuilder };
