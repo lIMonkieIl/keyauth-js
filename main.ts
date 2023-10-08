9e99528460f02b5b172c34a4486a7e603b3a3908
Monkie <a_brine@yahoo.com>
2023-10-08 16:30:05 +0100

    update cspell.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-import { ClientApi } from "@keyauthjs/client";
-async function main() {}
+import { SellerApi, EXPIRY, CHARACTER, EVENT_TYPE } from "@keyauthjs/seller";
+async function main() {
+    const sellerAPi = new SellerApi(
+        {
+            sellerKey: "9771968d4b6e487dc808f242428cff85",
+        },
+        { logger: { active: true, level: "debug" } },
+    );
+
+    sellerAPi.on(EVENT_TYPE.GET_USER_VAR, (data) => {
+        console.log(data);
+    });
+    sellerAPi.on(EVENT_TYPE.ERROR, (data) => {
+        console.log("ERROR: ", data);
+    });
+    await sellerAPi.user.var.get({ username: "chels", varName: "metaData" });
+}
+
 main();
